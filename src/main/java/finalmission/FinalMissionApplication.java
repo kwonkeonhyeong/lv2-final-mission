857f5b996cc96fa4518ae408c5832cb6eac8257e
kwonkeonhyeong <rjsgud19566@gmail.com>
2025-09-03 17:01:30 +0900

    chore: test

--- src/main/java/finalmission/FinalMissionApplication.java
+++ src/main/java/finalmission/FinalMissionApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FinalMissionApplication {
+    //    test
     public static void main(String[] args) {
         SpringApplication.run(FinalMissionApplication.class, args);
     }
