9aecda5c3bffe8c6ce1a8ed66479b1fed20a423e
kwonkeonhyeong <rjsgud19566@gmail.com>
2025-09-03 16:24:31 +0900

    chore: test commit

--- src/main/java/finalmission/FinalMissionApplication.java
+++ src/main/java/finalmission/FinalMissionApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FinalMissionApplication {
-    // ci cd
+    // ci cd test
     public static void main(String[] args) {
         SpringApplication.run(FinalMissionApplication.class, args);
     }
